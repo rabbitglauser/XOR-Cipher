199979df56876992d47be903b978467b5e28ba44
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-06-29 15:04:02 +0200

    Add inspection profiles, .gitignore, and main script
    This commit introduces several inspection profiles to assist in code quality checks and ensures certain IDE settings and local files are ignored by git. In addition, it incorporates a main script for the XOR cipher action.

---
+++ main.py
@@ -0,0 +1,21 @@
+def xor_cipher(text, key):
+    if isinstance(text, str):
+        text = text.encode()
+
+    while len(key) < len(text):
+        key += bytes(key)
+
+    key = key[:len(text)]
+
+    return bytes([x ^ y for x, y in zip(text, key)])
+
+
+original_text = "something"
+
+key = b'secret'
+
+encrypted = xor_cipher(original_text, key)
+print(encrypted)
+
+decrypted = xor_cipher(encrypted, key)
+print(decrypted.decode())
